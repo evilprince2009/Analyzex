dfebf3412783bb4faaabab0187a89860aa16c8ed
Ibne Nahian <evilprince2009@gmail.com>
2021-06-21 05:44:58 +0600

    dev

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,67 @@
 "use strict";
 const container = document.querySelector("body");
-const password = document.querySelector(".password input");
+const passwordInput = document.querySelector(".password input");
+const upper_label = document.querySelector("#upper-label");
+const lower_label = document.querySelector("#lower-label");
+const minimum_label = document.querySelector("#minimum-label");
+const numchar_label = document.querySelector("#numchar-label");
+const minimum_box = document.querySelector("#minimum");
+const lowercase_box = document.querySelector("#lowercase");
 const uppercase_box = document.querySelector("#uppercase");
-const passwordString = password.value;
-password.addEventListener("keyup", (e) => {
-    if (passwordString === passwordString.toUpperCase()) {
+window.addEventListener("DOMContentLoaded", () => {
+    passwordInput.focus();
+    upper_label.classList.remove("success");
+    lower_label.classList.remove("success");
+    minimum_label.classList.remove("success");
+});
+if (passwordInput.value === "" ||
+    !passwordInput.value ||
+    passwordInput.value == null) {
+    upper_label.classList.remove("success");
+    lower_label.classList.remove("success");
+    minimum_label.classList.remove("success");
+}
+container.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    const eventTarget = e.target;
+    const passwordString = eventTarget.value;
+    if (!checkCapital(passwordString)) {
+        uppercase_box.checked = false;
+    }
+    else {
         uppercase_box.checked = true;
+        upper_label.classList.add("success");
+    }
+    if (!checkLower(passwordString)) {
+        lowercase_box.checked = false;
+    }
+    else {
+        lowercase_box.checked = true;
+        lower_label.classList.add("success");
+    }
+    if (passwordString.length >= 8) {
+        minimum_box.checked = true;
+        minimum_label.classList.add("success");
+    }
+    else {
+        minimum_box.checked = false;
     }
 });
+const checkCapital = (input) => {
+    var _a;
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === ((_a = input[i]) === null || _a === void 0 ? void 0 : _a.toUpperCase())) {
+            return true;
+        }
+    }
+    return false;
+};
+const checkLower = (input) => {
+    var _a;
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === ((_a = input[i]) === null || _a === void 0 ? void 0 : _a.toLowerCase())) {
+            return true;
+        }
+    }
+    return false;
+};
--- source/script.ts
+++ source/script.ts
@@ -1,16 +1,88 @@
 const container: HTMLBodyElement = document.querySelector(
   "body"
 ) as HTMLBodyElement;
-const password: HTMLInputElement = document.querySelector(
+const passwordInput: HTMLInputElement = document.querySelector(
   ".password input"
+) as HTMLInputElement;
+const upper_label: HTMLLabelElement = document.querySelector(
+  "#upper-label"
+) as HTMLLabelElement;
+const lower_label: HTMLLabelElement = document.querySelector(
+  "#lower-label"
+) as HTMLLabelElement;
+const minimum_label: HTMLLabelElement = document.querySelector(
+  "#minimum-label"
+) as HTMLLabelElement;
+const numchar_label: HTMLLabelElement = document.querySelector(
+  "#numchar-label"
+) as HTMLLabelElement;
+const minimum_box: HTMLInputElement = document.querySelector(
+  "#minimum"
+) as HTMLInputElement;
+const lowercase_box: HTMLInputElement = document.querySelector(
+  "#lowercase"
 ) as HTMLInputElement;
 const uppercase_box: HTMLInputElement = document.querySelector(
   "#uppercase"
 ) as HTMLInputElement;
-const passwordString: string = password.value as string;
 
-password.addEventListener("keyup", (e: KeyboardEvent) => {
-  if (passwordString === passwordString.toUpperCase()) {
+window.addEventListener("DOMContentLoaded", () => {
+  passwordInput.focus();
+  upper_label.classList.remove("success");
+  lower_label.classList.remove("success");
+  minimum_label.classList.remove("success");
+});
+
+if (
+  passwordInput.value === "" ||
+  !passwordInput.value ||
+  passwordInput.value == null
+) {
+  upper_label.classList.remove("success");
+  lower_label.classList.remove("success");
+  minimum_label.classList.remove("success");
+}
+
+container.addEventListener("keyup", (e: KeyboardEvent) => {
+  e.preventDefault();
+  const eventTarget: HTMLInputElement = e.target as HTMLInputElement;
+  const passwordString: string = eventTarget.value;
+  if (!checkCapital(passwordString)) {
+    uppercase_box.checked = false;
+  } else {
     uppercase_box.checked = true;
+    upper_label.classList.add("success");
+  }
+
+  if (!checkLower(passwordString)) {
+    lowercase_box.checked = false;
+  } else {
+    lowercase_box.checked = true;
+    lower_label.classList.add("success");
+  }
+
+  if (passwordString.length >= 8) {
+    minimum_box.checked = true;
+    minimum_label.classList.add("success");
+  } else {
+    minimum_box.checked = false;
   }
 });
+
+const checkCapital: (input: string) => boolean = (input: string) => {
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === input[i]?.toUpperCase()) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const checkLower: (input: string) => boolean = (input: string) => {
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === input[i]?.toLowerCase()) {
+      return true;
+    }
+  }
+  return false;
+};
